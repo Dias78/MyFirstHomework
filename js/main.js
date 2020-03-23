bb3ac43384c383740255369df360ffbf10d7f184
Saukele <ssk.satai@gmail.com>
2020-03-23 23:09:47 +0600

    feat: added a map to the footer section& make responsive footer

--- js/main.js
+++ js/main.js
@@ -180,4 +180,40 @@ $(document).ready(function () {
   // маска для телефона
   $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
 
+  // создание яндекс карты
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [43.250744, 76.956847],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Этот ЖК оцеплен из-за коронавируса',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/marker.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+
+
+  });
+
 });
